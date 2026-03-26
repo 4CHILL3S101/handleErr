856fc9ce95a43345f12d3ccd531e80d73a044168
4CHILL3S101 <martdahao@gmail.com>
2026-03-27 01:01:29 +0800

    Initial release v1.0.0

--- checkerr.go
+++ checkerr.go
@@ -4,6 +4,9 @@ import (
 	"fmt"
 )
 
+// CheckErr checks if err is not nil.
+// Returns a boolean and a user-facing message.
+// If devMode is true, prints debug info and stack trace.
 func CheckErr(err error, userMsg string, dev bool) (bool, string) {
 	if err != nil {
 		if dev {
